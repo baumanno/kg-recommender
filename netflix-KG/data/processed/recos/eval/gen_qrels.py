39f5a38ff75c6b0b881433075b995a90461e931e
Anderson Rossanez <anderson.rossanez@ic.unicamp.br>
2024-04-13 17:58:50 -0300

    Adding NDCG analysis files

---
+++ netflix-KG/data/processed/recos/eval/gen_qrels.py
@@ -0,0 +1,32 @@
+from rdflib import Graph, URIRef
+
+output_filename = 'qrels.test'
+open(output_filename, 'w').close() # Clean if exists
+
+with open('../external_recos.txt', 'r') as f:
+    with open(output_filename, 'a') as out:
+
+        current_kg_file = None
+        kg = None
+        rank = 8808 # num. of nodes in the catalog
+        for line in f:
+            raw_line = line.strip().split()
+            queryid = raw_line[0]
+            documentid = raw_line[1]
+
+            if queryid != current_kg_file:
+                current_kg_file = queryid
+                kg = Graph()
+                kg.parse(f'../../{current_kg_file}.ttl', format="turtle")
+                rank = 8808
+
+            uri = URIRef(documentid)
+            if (uri, None, None) in kg or (None, None, uri) in kg:
+                print(f'Skipping already-existing {documentid} in {current_kg_file}')
+                continue
+
+            rank -= 1
+            out_line = f'{queryid} Q0 {documentid} {rank}'
+            out.write(f'{out_line}\n')
+            print(out_line)
+
---
+++ netflix-KG/data/processed/recos/eval/gen_results.py
@@ -0,0 +1,46 @@
+import os
+
+from argparse import ArgumentParser
+from pathlib import Path
+from sys import argv
+
+def main(args):
+    arg_p = ArgumentParser('python gen_results.py', description='Generates TREC_EVAL results file')
+    arg_p.add_argument('-m', '--metrics', type=str, default=None, help='Comma-separated metric list (e.g. \'degree,eigenvector,betweenness\').')
+
+    args = arg_p.parse_args(args[1:])
+
+    metrics = args.metrics
+    if metrics is None:
+        print('No metrics provided.')
+        exit(1)
+
+    metrics = metrics.split(',')
+
+    for metric in metrics:
+        output_filename = f'{metric}.results.test'
+        open(output_filename, 'w').close() # Clean if exists
+        
+        with open(output_filename, 'a') as out:
+            dirs = [d[0] for d in os.walk("..")]
+            for d in sorted(dirs):
+                if d == '..' or d == '../eval':
+                    continue
+
+                queryid = Path(d).stem
+                metric_recs_file = os.path.join(d, f'{metric}.txt')
+                with open(metric_recs_file, 'r') as f:
+                    rank = 0
+                    for line in f:
+                        contents = line.strip().split()
+                        documentid = contents[0]
+                        score = contents[1]
+                        rank += 1
+                    
+                        out_line = f'{queryid} Q0 {documentid} {rank} {score} {metric.upper()}'
+                        out.write(f'{out_line}\n')
+                        print(out_line)
+
+if __name__ == '__main__':
+    exit(main(argv))
+
